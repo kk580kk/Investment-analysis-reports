dbb8dce8f9a8d2e0e0371bca8e677209c4421ed8
huangjie <huangjie@baosight.com>
2026-06-04 14:29:01 +0800

    feat: markdown-it 渲染报告正文 + highlight.js 代码高亮

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { createPinia } from 'pinia';
 import router from './router';
 import App from './App.vue';
 import './assets/main.css';
+import 'highlight.js/styles/github-dark.css';
 
 const app = createApp(App);
 app.use(createPinia());
